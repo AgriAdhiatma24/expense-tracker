e07073d94bfeb8bcf786a30c7d4bcaa23537eeb4
unknown <agriadhiatma@gmail.com>
2023-08-09 13:43:34 +0700

    feat: add sidebar menu

---
+++ public/scripts.js
@@ -0,0 +1,11 @@
+//Animation Active On Sidebar
+const sidebarMenu = document.querySelector(".sidebar");
+const btns = sidebarMenu.getElementsByTagName("a");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
