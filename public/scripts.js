995134397457b6637f90f51d5aae6c5a7487e00a
unknown <agriadhiatma@gmail.com>
2023-08-10 13:27:48 +0700

    feat: add theme-toggler for dark/light mode

--- public/scripts.js
+++ public/scripts.js
@@ -3,9 +3,18 @@ const sidebarMenu = document.querySelector(".sidebar");
 const btns = sidebarMenu.getElementsByTagName("a");
 
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+  btns[i].addEventListener("click", function () {
     const current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
+
+// Dark Theme Toggler
+const themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-var");
+  themeToggler.querySelector(".light-mode").classList.toggle("active");
+  themeToggler.querySelector(".dark-mode").classList.toggle("active");
+});
